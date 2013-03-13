977bc24b849cf89dd83b0b007cc3fe6d9b6b2157
Francois Harbec <fharbec@gmail.com>
2013-03-13 13:57:43 -0700

    use of active scope can dramatically slow down performance
    not needed if the products are only indexed if they are active

--- lib/spree/sunspot/search.rb
+++ lib/spree/sunspot/search.rb
@@ -62,11 +62,15 @@ module Spree
 
       protected
       def get_base_scope
-        base_scope = Spree::Product.active
-        base_scope = base_scope.in_taxon(taxon) unless taxon.blank?
+        base_scope = Spree::Product
+
+        unless taxon.blank?
+          base_scope = base_scope.active
+          base_scope = base_scope.in_taxon(taxon)
+        end
+
         base_scope = get_products_conditions_for(base_scope, keywords)
         base_scope = base_scope.on_hand unless Spree::Config[:show_zero_stock_products]
-
         base_scope = add_search_scopes(base_scope)
       end
 
